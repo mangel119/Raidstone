2b3fe5bcb16d4f013791a2128a88c0c357374f52
mangel119 <miguel.sanabria@uao.edu.co>
2021-11-20 03:27:20 -0500

    Explosion Enemigos

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject explosion;
     public AudioSource playSound;
+    public AudioSource explode;
     public float health = 100;
+    public bool alreadyPlayed = false;
     public void TakeDamage(float damage)
     {
         playSound.Play();
         health -= damage;
         if(health <= 0){
-            Destroy(gameObject);
+            if (!alreadyPlayed)
+            {
+                explode.Play();
+                alreadyPlayed=true;
+            }
+            
+            Instantiate(explosion,transform.position, Quaternion.identity);
+            Destroy(gameObject,2.0f);
         }
 
     }
